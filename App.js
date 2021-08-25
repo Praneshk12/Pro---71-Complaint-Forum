0147e57f15f6b57dd9540356ed963013e9d6a918
Praneshk12 <77246096+Praneshk12@users.noreply.github.com>
2021-08-24 18:50:36 -0700

    Update App.js

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 import {createAppContainer} from 'react-navigation';
 import {createBottomTabNavigator} from 'react-navigation-tabs' 
 import WriteComplaint from './WriteComplaint'
-import ReadComplaint from './ReadComplaint'
+import ReadComplaint from './ReadCompaint'
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 export default class App extends React.Component {
